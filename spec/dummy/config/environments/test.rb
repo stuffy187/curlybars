2efe1775ac92faa2125684c9fcdb2beef6492816
stuffy187 <ehuckaby1488@icloud.com>
2020-01-15 13:45:10 -0500

    Delete test.rb

--- spec/dummy/config/environments/test.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.configure do
-  config.eager_load = false
-  config.action_dispatch.show_exceptions = false
-  config.active_support.deprecation = :raise
-  config.secret_key_base = "yolo"
-end
