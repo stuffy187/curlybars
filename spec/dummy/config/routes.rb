4a74d2f7838ceaee27793127ff54da5c0c33869d
stuffy187 <ehuckaby1488@icloud.com>
2020-01-15 13:44:20 -0500

    Delete routes.rb

--- spec/dummy/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  root to: "dashboards#show"
-  get "/articles/1", to: "articles#show"
-  get "/categories", to: "categories#index"
-  get "/welcome", to: "welcome#show"
-end
