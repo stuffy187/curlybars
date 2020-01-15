e684c48045e4a0efd6fab2144e80661d8880cad8
stuffy187 <ehuckaby1488@icloud.com>
2020-01-15 13:38:07 -0500

    Delete dependency_tracker.rb

--- lib/curlybars/dependency_tracker.rb
+++
@@ -1,8 +0,0 @@
-module Curlybars
-  class DependencyTracker
-    def self.call(path, template)
-      presenter = Curlybars::Presenter.presenter_for_path(path)
-      presenter.dependencies.to_a
-    end
-  end
-end
