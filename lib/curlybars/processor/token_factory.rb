d952dc7d46023d8e51fc869c0bd1e46a60f7038e
stuffy187 <ehuckaby1488@icloud.com>
2020-01-15 13:42:23 -0500

    Delete token_factory.rb

--- lib/curlybars/processor/token_factory.rb
+++
@@ -1,9 +0,0 @@
-module Curlybars
-  module Processor
-    module TokenFactory
-      def create_token(type, value, position)
-        RLTK::Token.new(type, value, position)
-      end
-    end
-  end
-end
